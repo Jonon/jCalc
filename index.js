5214d7acde0123f54ee9e8ca6856e97d9dfd020d
Jonathan <jonathancjonsson@gmail.com>
2025-07-18 17:14:16 +0200

    Implement opperation handler for basic calculations

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ function subtract(a, b) {
 function multiply(a, b) {
 	return a * b;
 }
+
+function divide(a, b) {
+	return a / b;
+}
+
+let a;
+let b;
+let operator;
+
+function operate(operator, a, b) {
+	switch (operator) {
+		case "add":
+			return add(a, b);
+		case "subtract":
+			return subtract(a, b);
+		case "multiply":
+			return multiply(a, b);
+		case "divide":
+			return divide(a, b);
+	}
+}
+
+operate(operator, a, b);
